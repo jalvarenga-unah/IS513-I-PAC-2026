bc1f74fd801258eef3e433912d4195ca6fa0e692
jalvarenga11 <juan.alvarenga@ess.hn>
2026-02-01 10:52:33 -0600

    conceptos para condiciones, operadores == y ===

---
+++ intro-js/03-condiciones.js
@@ -0,0 +1,16 @@
+//todo nulish: null o undefined
+//? falsey: false, 0, "", NaN (not a number)
+//* truthy: true, 1, "a", [], {}
+
+
+const valor = []
+
+if (NaN == NaN) {
+
+    console.log("Es truthy");
+
+} else {
+
+    console.log("Es falsey");
+}
+
