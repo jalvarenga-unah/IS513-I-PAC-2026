6a47e78e97932b75df79c932bd2748cc6a595f9d
jalvarenga11 <juan.alvarenga@ess.hn>
2026-02-05 17:56:44 -0600

    manejo de callbacks

---
+++ intro-js/07-callbacks.js
@@ -0,0 +1,54 @@
+const { cpSync } = require("fs")
+
+
+const numeros = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10]
+
+
+// for (let i = 0; i < numeros.length; i++) {
+//     console.log(numeros[i])
+// }
+
+// for (const [index, value] of numeros.entries()) {
+//     console.log(`indice: ${index}, valor: ${value}`)
+// }
+
+
+
+// numeros.forEach(console.log)
+numeros.forEach((value, index, array) => {
+
+    if (value % 2 == 0)
+        console.log(value, index, array)
+})
+
+console.log('------------------')
+
+const nueva_lista = numeros.map((value, index, array) => {
+    if (value % 2 == 0)
+        return value
+})
+
+console.log(nueva_lista)
+
+
+const nueva_lista_2 = numeros.filter((value, index, array) => {
+
+    if (value % 2 == 0)
+        return value
+})
+
+
+console.log(nueva_lista_2)
+
+const nueva_lista_3 = numeros.some((value, index, array) => {
+
+    return value % 2 == 0
+})
+
+console.log(nueva_lista_3)
+
+const nueva_lista_4 = numeros.every((value, index, array) => {
+    return value % 2 == 0
+})
+
+console.log(nueva_lista_4)
