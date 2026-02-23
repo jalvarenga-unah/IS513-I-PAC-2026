86caa47069140d5248e71d6f7098e2afdaeaef7f
jalvarenga11 <juan.alvarenga@ess.hn>
2026-02-23 17:22:50 -0600

    creacion de nuestro primer servidor web con node

--- intro-node/index.js
+++ intro-node/index.js
@@ -1,1 +1,15 @@
-console.log("Hola, mundo!");
+import { createServer } from 'node:http'
+
+// la instancia del servidor
+const server = createServer((req, res) => {
+    res.write('Hola mundo')
+    res.end()
+})
+
+// definir un puerto
+const PORT = 3000 // 1- 65535
+
+// empezar a escuchar el puerto
+server.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
